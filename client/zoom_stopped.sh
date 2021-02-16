95e4409c77076bff47698c1d730169ab07042dec
James Woglom <j@wogloms.net>
2021-02-16 02:45:40 -0500

    make ip prefix configurable

--- client/zoom_stopped.sh
+++ client/zoom_stopped.sh
@@ -3,7 +3,7 @@ FOLDER=$WMSCRIPTS/zoom-tools/client/
 source $FOLDER/secrets.py
 
 # Runs when a zoom meeting is started, and registers the local IP if present
-LOCAL_IP=$(ifconfig|grep 192.168|sed 's/\(.*\)inet \(.*\) netmask \(.*\)/\2/g')
+LOCAL_IP=$(ifconfig|grep $IP_PREFIX|sed 's/\(.*\)inet \(.*\) netmask \(.*\)/\2/g')
 if [[ "$LOCAL_IP" != "" ]]; then
     (echo zoom_stopped; date; curl -X POST -d "ip=$LOCAL_IP&name=$(hostname)&token=$SELF_TOKEN" $ZOOMTOOLS_URL/unregister; echo) >> $FOLDER/log.log
 fi
